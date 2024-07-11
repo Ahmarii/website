98454e7721b3a5b0ac4205408e68833ec83d4a19
Ahmarii <ahmari.work@gmaiil.com>
2024-07-11 14:41:39 +0700

    try to fix routing issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ahmarii.github.io',
+  site: 'https://ahmarii.github.io/website',
   integrations: [mdx(), sitemap(), tailwind()]
 });
