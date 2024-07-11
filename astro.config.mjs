9ec6436a660e928ba6b6a0cc3cda6a85158b0c17
Ahmarii <ahmari.work@gmaiil.com>
2024-07-11 14:46:26 +0700

    Another Attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ahmarii.github.io/website',
+  site: 'https://ahmarii.github.io',
+  base: 'website',
   integrations: [mdx(), sitemap(), tailwind()]
 });
